5b742b1a2f54ee001c8aa354b3fc3384b1d2d250
HostalgiC <kheawkhumhassadin@gmail.com>
2026-02-11 09:07:19 +0000

    Function

--- main.c
+++ main.c
@@ -1,29 +1,25 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "Node.h"
 #include "Queue.h"
+#include <string.h>
 
-int main(int argc , char **argv) {
-  // สร้างและกำหนดค่าเริ่มต้นให้ Queue
-  Queue q;
-  q.headPtr = NULL;
-  q.tailPtr = NULL;
-  q.size = 0;
+int main(int argc, char *argv[]) {
+    Queue q;
+    initQueue(&q);
 
-  int i;
-  for(i = 1; i < argc; i++){
-    if(strcmp(argv[i], "x") == 0){
-      // เมื่อเจอ x ให้สั่ง dequeue (ฟังก์ชันจะ print ผลลัพธ์ให้เอง)
-      dequeue_struct(&q);
+    for (int i = 1; i < argc; i++) {
+
+        if (strcmp(argv[i], "x") == 0) {
+            int value = dequeue(&q);
+            if (value == -1) {
+                printf("empty queue\n");
+            } else {
+                printf("%d\n", value);
+            }
+        } 
+        else {
+            int num = atoi(argv[i]);
+            enqueue(&q, num);
+        }
     }
-    else {
-      // ถ้าไม่ใช่ x ให้แปลงเป็นตัวเลขแล้ว enqueue
-      enqueue_struct(&q, atoi(argv[i]));
-    }
-  }
 
-  return 0;
+    return 0;
 }
-
-//rtftgfgf
